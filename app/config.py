1ef5dbb2d7ea4f8d348a50092320fb7bb56bfe5f
gil <elfgil2003@gmail.com>
2026-02-05 15:00:26 +0900

    컨피그 변경

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 # config.py
-OPENAPI_BASE = "https://openapi.chzzk.naver.com"
+import os
 
+OPENAPI_BASE = "https://openapi.chzzk.naver.com"
 
 # 치지직 API 토큰 정보
 CLIENT_ID = '9ebe5989-29c5-4ab5-a4bc-19071b95245a'
-CLIENT_SECRET = '960SVaUEZYimSal82dfPQ35hDSdXhxZKhXsekIiYqy4'
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URL = 'https://milkywaybot.cloud/auth/callback'
