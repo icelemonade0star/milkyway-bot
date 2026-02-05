a6ff3ab83f681f38639c838a9d48389a87eb7b2e
gil <elfgil2003@gmail.com>
2026-02-05 15:08:35 +0900

    추가 변경

--- app/chzzk/handler/chat_handler.py
+++
@@ -1,8 +0,0 @@
-from app.chzzk.base_socket_client import BaseSocketEventHandler
-
-class ChatHandler(BaseSocketEventHandler):
-    async def handle_event(self, event_type: str, data: dict) -> None:
-        channel_id = data.get("channelId")
-        nickname = data.get("senderNickname")
-        content = data.get("content")
-        print(f"💬 [{channel_id}] {nickname}: {content}")
