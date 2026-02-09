c7151683486c5b87cd2b67ff3994bbfd931a1f75
gil <elfgil2003@gmail.com>
2026-02-09 18:37:58 +0900

    쿼리변경3

--- app/db/queries/auth_token_insert.sql
+++ app/db/queries/auth_token_insert.sql
@@ -5,10 +5,10 @@ INSERT INTO auth_token (
     refresh_token, 
     expires_at
 ) VALUES (
-    :channel_id::text,
-    :channel_name::text,
-    :access_token::text,
-    :refresh_token::text,
+    (:channel_id)::text,
+    (:channel_name)::text,
+    (:access_token)::text,
+    (:refresh_token)::text,
     CURRENT_TIMESTAMP + INTERVAL '24 hours'
 )
 RETURNING channel_id, channel_name, expires_at;
