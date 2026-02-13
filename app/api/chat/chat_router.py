b678b2757f0893e6a9e94ff27f7c944506125523
gil <elfgil2003@gmail.com>
2026-02-13 13:33:40 +0900

    채팅 전송 주석처리

--- app/api/chat/chat_router.py
+++ app/api/chat/chat_router.py
@@ -7,23 +7,23 @@ from app.api.chat.chzzk_sessions import ChzzkSessions
 
 chat_router = APIRouter(prefix="/chat", tags=["chat"])
 
-@chat_router.get("/send")
-async def send_chat_message(
-    channel_id: str,
-    message: str,
-    db: AsyncSession = Depends(get_async_db)
-):
-    auth_service = AuthService(db)
-    auth_data = await auth_service.get_auth_token_by_id(channel_id)
+# @chat_router.get("/send")
+# async def send_chat_message(
+#     channel_id: str,
+#     message: str,
+#     db: AsyncSession = Depends(get_async_db)
+# ):
+#     auth_service = AuthService(db)
+#     auth_data = await auth_service.get_auth_token_by_id(channel_id)
 
-    if not auth_data:
-        return {"error": "채널 ID에 해당하는 인증 정보가 없습니다."}
+#     if not auth_data:
+#         return {"error": "채널 ID에 해당하는 인증 정보가 없습니다."}
     
-    access_token = auth_data.access_token
+#     access_token = auth_data.access_token
 
-    result = await ChzzkSessions.send_chat(access_token, message)
+#     result = await ChzzkSessions.send_chat(access_token, message)
     
-    if not result:
-        return {"error": "채팅 전송에 실패했습니다."}
-    return {"message": "채팅 전송에 성공했습니다."}
+#     if not result:
+#         return {"error": "채팅 전송에 실패했습니다."}
+#     return {"message": "채팅 전송에 성공했습니다."}
     
