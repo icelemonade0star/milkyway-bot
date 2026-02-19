4b49585be463cc93a2e259018f8e9879014d4e49
gil <elfgil2003@gmail.com>
2026-02-19 19:05:52 +0900

    토큰관련 추가변경

--- app/db/queries/auth_token_insert.sql
+++ app/db/queries/auth_token_insert.sql
@@ -11,4 +11,10 @@ INSERT INTO auth_token (
     (:refresh_token)::text,
     CURRENT_TIMESTAMP + INTERVAL '24 hours'
 )
+ON CONFLICT (channel_id) DO UPDATE SET
+    channel_name = EXCLUDED.channel_name,
+    access_token = EXCLUDED.access_token,
+    refresh_token = EXCLUDED.refresh_token,
+    expires_at = EXCLUDED.expires_at,
+    updated_at = CURRENT_TIMESTAMP
 RETURNING channel_id, channel_name, expires_at;
