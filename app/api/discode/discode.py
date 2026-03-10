7126c1fff993be2587aa9ae10d0ff4512cfcc4d3
gil <elfgil2003@gmail.com>
2026-03-10 16:58:28 +0900

    디스코드 서버연결 관련 로직 변경, 로그추가

--- app/api/discode/discode.py
+++ app/api/discode/discode.py
@@ -1,7 +1,9 @@
 import os
+import logging
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from app.api.discode.chzzk_notifications import ChzzkNotification
 
 load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
@@ -11,6 +13,20 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
 @bot.event
 async def on_ready():
     print(f"Milkyway Bot ({bot.user}) 로그인 완료!")
+
+
+async def start_discord_bot(token: str):
+    try:
+        # discord.py 내부 통신 로그 켜기
+        discord.utils.setup_logging(level=logging.INFO)
+
+        await bot.add_cog(ChzzkNotification(bot))
+        
+        print("⏳ [Discord] 서버로 연결 시도 중...")
+        await bot.start(token)
+        
+    except Exception as e:
+        print(f"🚨 [Discord] 봇 실행 중 치명적 에러 발생: {e}")
 
 if __name__ == "__main__":
     if discord_token:
