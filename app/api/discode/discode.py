f0532201f3e86c62d968e32aaed79bf7ff678d12
gil <elfgil2003@gmail.com>
2026-03-10 16:24:55 +0900

    임포트 수정

--- app/api/discode/discode.py
+++ app/api/discode/discode.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from app.api.discode.chzzk_notifications import ChzzkNotification 
 
 load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
